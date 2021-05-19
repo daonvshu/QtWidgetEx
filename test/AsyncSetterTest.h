309fb869a9e009889eca1cc5aa17a1bf57e167c1
hydrone <1286772338@qq.com>
2021-05-19 10:42:00 +0800

    添加AsyncSetter速度测试

--- test/AsyncSetterTest.h
+++ test/AsyncSetterTest.h
@@ -4,11 +4,20 @@
 
 using namespace _widget_ex;
 class AsyncSetterTest : public AsyncDataSetter {
+    Q_OBJECT
+
 public:
     using AsyncDataSetter::AsyncDataSetter;
 
     void start();
 
+signals:
+    void postValue3(const QVariant&);
+
 protected:
     void setDataInMainThread(const QVariant& value) override;
+
+    void setValue2(const QVariant&);
+
+    void setValue3(const QVariant&);
 };
